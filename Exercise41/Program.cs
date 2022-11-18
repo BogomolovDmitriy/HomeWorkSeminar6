e1fda8204ebaaa9e6ac5f34ce8176cc7da1c0426
Dmitriy <dimgod80@yandex.ru>
2022-11-18 20:12:15 +0800

    add Exercise41

---
+++ Exercise41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите числа через зарятую: ");
+string numbers = Console.ReadLine();
+
+int[] numbersArray = numbers.Split(',').Select(int.Parse).ToArray();
+int countNumbers =  CountingPositiveNumbers(numbersArray);
+Console.WriteLine($"Пользователем введено {countNumbers} положительных чисел.");
+
+
+int CountingPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+
+
